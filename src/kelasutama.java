0c1057f15916b7ff82ec3fbf1cbdfb2b05e5716f
GentraAW <gentraaw@gmail.com>
2020-03-29 20:22:50 +0700

    first commit

---
+++ src/kelasutama.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gentra
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        //System.out.println("Hallo Dunia");
+        //hidupkan object
+        //frameutama futama = new frameutama();
+        //futama.tampil("Program Pertamaku");
+        futama2 ut = new futama2();
+        ut.setVisible(true);
+    }
+}
